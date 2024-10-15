c9d3f107b837e90cf7b0d4a566ba703325c0eeda
stLmpp <gui.stlmpp@gmail.com>
2024-10-15 20:21:28 -0300

    feat: migrate region

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { AchievementsCoreAdapter } from '@st-achievements/core';
 import { StFirebaseApp } from '@st-api/firebase';
 import { GetWorkoutsController } from './user-workout/get-workouts.controller.js';
 import { GetWorkoutController } from './user-workout/get-workout.controller.js';
+import { setGlobalOptions } from 'firebase-functions/v2';
+
+setGlobalOptions({
+  region: 'southamerica-east1',
+});
 
 const app = StFirebaseApp.create({
   adapter: new AchievementsCoreAdapter({
