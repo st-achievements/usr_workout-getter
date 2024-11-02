db847f85e622a655ac2d414a353e1fad8e04f408
stLmpp <gui.stlmpp@gmail.com>
2024-11-02 00:25:07 -0300

    chore: update dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,5 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   ...tsupConfig,
   dts: false,
-  experimentalDts: false,
   external: ['vitest'],
-  plugins: tsupConfig.plugins?.filter((plugin) => plugin.name !== 'dts-fix'),
 });
